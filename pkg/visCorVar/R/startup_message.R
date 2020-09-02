5e4c8a07c546e4d248910c6d5e0e1c169d270e12
brunin <brunin@610450c7-e488-4fb5-a9aa-83bf88e67e81>
2020-09-02 11:53:59 +0000

    add launch message when the package is loaded
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/viscorvar@14 610450c7-e488-4fb5-a9aa-83bf88e67e81

---
+++ pkg/visCorVar/R/startup_message.R
@@ -0,0 +1,17 @@
+.onAttach <- function(libname, pkgname)
+{ 
+  packageStartupMessage("\nLoaded visCorVar ",as.character(packageDescription("visCorVar")[["Version"]]),
+                                                               
+                                                               "\n https://gitlab.com/bilille/viscorvar/-/blob/master/vignettes/visCorVar.pdf "
+
+  )}
+
+# .onAttach <- function(libname, pkgname){ packageStartupMessage("\nLoaded mixOmics ",as.character(packageDescription("mixOmics")[["Version"]]),
+#                                                                
+#                                                                "\n\nThank you for using mixOmics!",
+#                                                                "\n\nHow to apply our methods: http://www.mixOmics.org for some examples.",
+#                                                                "\nQuestions or comments: email us at mixomics[at]math.univ-toulouse.fr  ",
+#                                                                "\nAny bugs? https://bitbucket.org/klecao/package-mixomics/issues",
+#                                                                "\nCite us:  citation('mixOmics')"
+#                                                                
+# )}
